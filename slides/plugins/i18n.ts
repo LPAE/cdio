b4c9c8820272ee2c000a96518c984dec4d8f277a
GeovaneSchmitz <contato@geovanems.com.br>
2021-08-10 23:41:28 -0300

    Fix folder

--- slides/plugins/i18n.ts
+++
@@ -1,9 +0,0 @@
-import { createI18n } from 'vue-i18n'
-import messages from '@/lang/en.json'
-
-
-const i18n = createI18n({
-  locale: 'en', // set locale
-  fallbackLocale: 'en',
-  messages // set locale messages
-})
