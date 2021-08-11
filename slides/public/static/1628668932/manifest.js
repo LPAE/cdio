867b56ceebc58707d30432b77de6321a1b3d3f76
Fábio Cabral Pacheco <fcabralpacheco@gmail.com>
2021-08-11 06:32:09 -0300

    Fix index path.

--- slides/public/static/1628668932/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fcdio\u002Fslides\u002Fpresentation\u002F3"]})
---
+++ slides/public/static/1628673879/cdio/slides/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/cdio/slides", {data:[{}],fetch:{},mutations:[]});
